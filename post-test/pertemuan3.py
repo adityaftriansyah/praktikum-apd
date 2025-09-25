e624aa9f2c663d7f1f58b3c65cd5568a70fe0b78
adityaftriansyah <adityaftriansyh@gmail.com>
2025-09-25 14:10:18 +0800

    Add files via upload

---
+++ post-test/pertemuan3.py
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+
+
+
+nilai = 70
+
+if nilai >  75:
+    print('nilai A')
+elif nilai > 65:
+    print("nilai B")
+else:
+    print("nilai C")
+
+suhu= 35
+
+if suhu >= 35:
+    print('panas')
+if suhu > 30:
+    print('sauna')
+elif suhu >20:
+    print('normal')
+else:
+    print('dingin')
